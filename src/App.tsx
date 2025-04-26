5c32add459ea32f0c792574468941cabfccc4ea6
Alokzh <zhalok24@gmail.com>
2025-04-26 18:44:28 +0530

    Implement basic route setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
-function App() {
-  return <>CF Website !</>;
-}
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<div>Home Page</div>} />
+            <Route path="/resources" element={<div>Resources Page</div>} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
+};
 
 export default App;

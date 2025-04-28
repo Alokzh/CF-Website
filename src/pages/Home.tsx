63202273460ebe3814e4f9ad8d0bc78e80b754ca
Alokzh <zhalok24@gmail.com>
2025-04-28 21:44:33 +0530

    Fetch and display problems from db.json

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
+
+interface Problem {
+  id: number;
+  problemStatement: string;
+  problemLink: string;
+  solutionLink: string;
+  rating: number;
+}
+
 const Home: React.FC = () => {
+  const [problems, setProblems] = useState<Problem[]>([]);
+
+  useEffect(() => {
+    fetch("/db.json")
+      .then((res) => res.json())
+      .then((data) => setProblems(data))
+      .catch((error) => console.error("Error fetching problems:", error));
+  }, []);
+
   return (
     <div className="container">
-      <h1>Codeforces Problems</h1>
+      <p style={{ marginLeft: "38px" }}>
+        <strong>Tip:</strong> Going to solution without attempting the question
+        is waste of your time
+      </p>
+      <br />
       <table className="table">
         <thead>
           <tr>
@@ -12,7 +35,33 @@ const Home: React.FC = () => {
             <th>Solved</th>
           </tr>
         </thead>
-        <tbody>{/* Problem data will be displayed here */}</tbody>
+        <tbody>
+          {problems.map((problem, index) => (
+            <tr key={problem.id}>
+              <td>{index + 1}.</td>
+              <td>
+                <a
+                  href={problem.problemLink}
+                  style={{ textDecoration: "none", color: "blue" }}
+                >
+                  {problem.problemStatement}
+                </a>
+              </td>
+              <td>
+                <a
+                  href={problem.solutionLink}
+                  style={{ textDecoration: "none", color: "blue" }}
+                >
+                  Solution
+                </a>
+              </td>
+              <td>{problem.rating}</td>
+              <td>
+                <input type="checkbox" />
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );

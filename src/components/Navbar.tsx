7557ebc8ab3688642213498eff170ed51f2ea142
Alokzh <zhalok24@gmail.com>
2025-05-01 17:45:53 +0530

    Added Github icon in Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,24 @@
+import { FaGithub } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 
 const Navbar: React.FC = () => {
-  const navigate = useNavigate();
-  
-  return (
-    <nav className="navbar">
-      <div className="logo">
-        <p onClick={() => navigate("/")}>CF Website</p>
-      </div>
-      <div className="links">
-        <p onClick={() => navigate("/resources")}>Resources</p>
-      </div>
-    </nav>
-  );
+    const navigate = useNavigate();
+    
+    return (
+        <nav className="navbar">
+            <div className="logo">
+                <p onClick={() => navigate("/")}>CF Website</p>
+            </div>
+            <div className="links">
+                <p onClick={() => navigate("/resources")}>Resources</p>
+            </div>
+            <div className="github-icon">
+                <a href="https://github.com/Alokzh/CF-Website">
+                    <FaGithub />
+                </a>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
